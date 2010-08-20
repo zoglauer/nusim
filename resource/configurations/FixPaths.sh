1a49e15b1827b0917a7848880800d2519075c341
Andreas Zoglauer <zoglauer@berkeley.edu>
2010-08-20 17:33:23 +0000

    CHG: Improved path fixer CHG: Fixed the paths in the Westerlund 1 example
    git-svn-id: https://elves.srl.caltech.edu/svn/nusim/trunk@29 f61b1093-7add-4ab6-ac1b-eb05caf01ebb

--- resource/configurations/FixPaths.sh
+++ resource/configurations/FixPaths.sh
@@ -2,74 +2,38 @@
 
 help() {
     echo ""
-    echo "Replace script for NuSIM configuration file";
-    echo "  based upon MEGAlib's replace script." 
+    echo "FixPath script for NuSIM configuration file";
+    echo "This script looks for the \$NUSIM path and replaces it in the following manner:"
+    echo "/home/andreas/nusim/Myfile.dat"
+    echo "with: "
+    echo "\$NUSIM/MyFile.dat"
     echo "";
-    echo "Usage: replace 'original string' 'new string' filename1 filename2 ...";
-    echo "";
-    echo "Remark 1: The quotation marks are only necessary if the string contains space characters.";
+    echo "Usage: FixPath filename1 filename2 ...";
     echo "";
 }
 
 
 
-if [ $# -lt 3 ] ; then
+if [ $# -lt 1 ] ; then
     help; 
     exit 0;
 fi;
 
-
-start=$1
-shift
-stop=$1
-shift
 files=" "
 while [ $# -gt 0 ] ; do
     files="$files $1"
     shift
 done
 
-types="cfg"
+start="$NUSIM"
+stop="\$NUSIM"
 
-
-
-# Do some sanity checks on the input:
-substr() {
-  echo "$1" | cut -c$2-`expr $2 + $3 - 1`
-}
-
-length=${#start}
-
-last='-'
-for inc in `seq 1 $length`; do
-    char=`substr "${start}" ${inc} 1`
-    if ([ "$char" == "/" ] && [ "$last" != "\\" ]); then 
-        echo "Error: All \"/\" must be preceded by a \"\\\"!"
-        exit 1;
-    fi
-    last=$char
-done
-
-length=${#stop}
-
-last='-'
-for inc in `seq 1 $length`; do
-    char=`substr "${stop}" ${inc} 1`
-    if ([ "$char" == "/" ] && [ "$last" != "\\" ]); then 
-        echo "Error: All \"/\" must be preceded by a \"\\\"!"
-        exit 1;
-    fi
-    last=$char
-done
-
-current=`pwd`
-
-echo $start $stop $files
+echo "Replace $start with $stop"
 
 for file in $files; do
     echo "  Analysing file: $file";
     newfile="/tmp/"$file".new";
-    sed "s/$start/$stop/g" $file > $newfile;
+    sed "s|$start|$stop|g" $file > $newfile;
     ret=`diff $file $newfile`;
     if (`test -z "$ret"`); then
         echo "    No modification necessary";
