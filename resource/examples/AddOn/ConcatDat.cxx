69e965e742ba1bd0a84ea6ee32287d4470a834e7
Andreas Zoglauer <zoglauer@berkeley.edu>
2011-04-07 16:36:39 +0000

    ADD: Program to concat dat files (including smooth time and ID continuations)
    git-svn-id: https://elves.srl.caltech.edu/svn/nusim/trunk@239 f61b1093-7add-4ab6-ac1b-eb05caf01ebb

---
+++ resource/examples/AddOn/ConcatDat.cxx
@@ -0,0 +1,270 @@
+/* 
+ * ConcatDat.cxx
+ *
+ *
+ * Copyright (C) by Andreas Zoglauer.
+ * All rights reserved.
+ *
+ *
+ * This code implementation is the intellectual property of
+ * Andreas Zoglauer.
+ *
+ * By copying, distributing or modifying the Program (or any work
+ * based on the Program) you indicate your acceptance of this statement,
+ * and all its terms.
+ *
+ */
+
+// Standard
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <csignal>
+#include <map>
+using namespace std;
+
+// ROOT
+#include <TApplication.h>
+#include <TStyle.h>
+#include <TH1.h>
+#include <TMath.h>
+#include <TCanvas.h>
+#include <TMarker.h>
+#include <TString.h>
+
+// NuSIM
+#include "NExtractFitsImage.h"
+#include "MFunction2D.h"
+#include "NModuleEventLoader.h"
+#include "NModuleEventSaver.h"
+#include "NTime.h"
+#include "NSatellite.h"
+
+
+/******************************************************************************/
+
+class ConcatDat
+{
+public:
+  /// Default constructor
+  ConcatDat();
+  /// Default destructor
+  ~ConcatDat();
+  
+  /// Parse the command line
+  bool ParseCommandLine(int argc, char** argv);
+  /// Analyze whatever needs to be analyzed...
+  bool Analyze();
+  /// Interrupt the analysis
+  void Interrupt() { m_Interrupt = true; }
+
+
+private:
+  /// True, if the analysis needs to be interrupted
+  bool m_Interrupt;
+
+  /// Input file names
+  vector<TString> m_InputFileNames;
+ 
+  /// Output file name
+  TString m_OutputFileName;
+};
+
+/******************************************************************************/
+
+
+/******************************************************************************
+ * Default constructor
+ */
+ConcatDat::ConcatDat() : m_Interrupt(false)
+{
+  gStyle->SetPalette(1, 0);
+}
+
+
+/******************************************************************************
+ * Default destructor
+ */
+ConcatDat::~ConcatDat()
+{
+  // Intentionally left blanck
+}
+
+
+/******************************************************************************
+ * Parse the command line
+ */
+bool ConcatDat::ParseCommandLine(int argc, char** argv)
+{
+  ostringstream Usage;
+  Usage<<endl;
+  Usage<<"  Usage: ConcatDat <options>"<<endl;
+  Usage<<"    General options:"<<endl;
+  Usage<<"         -i:   Input file name"<<endl;
+  Usage<<"         -o:   Output file name"<<endl;
+  Usage<<"         -h:   print this help"<<endl;
+  Usage<<endl;
+
+  string Option;
+
+  // Check for help
+  for (int i = 1; i < argc; i++) {
+    Option = argv[i];
+    if (Option == "-h" || Option == "--help" || Option == "?" || Option == "-?") {
+      cout<<Usage.str()<<endl;
+      return false;
+    }
+  }
+
+  // Now parse the command line options:
+  for (int i = 1; i < argc; i++) {
+		Option = argv[i];
+
+		// First check if each option has sufficient arguments:
+		// Single argument
+    if (Option == "-i" || Option == "-o") {
+			if (!((argc > i+1) && argv[i+1][0] != '-')){
+				cout<<"Error: Option "<<argv[i][1]<<" needs a second argument!"<<endl;
+				cout<<Usage.str()<<endl;
+				return false;
+			}
+		} 
+		// Multiple arguments_
+		//else if (Option == "-??") {
+		//	if (!((argc > i+2) && argv[i+1][0] != '-' && argv[i+2][0] != '-')){
+		//		cout<<"Error: Option "<<argv[i][1]<<" needs two arguments!"<<endl;
+		//		cout<<Usage.str()<<endl;
+		//		return false;
+		//	}
+		//}
+
+		// Then fulfill the options:
+    if (Option == "-i") {
+      m_InputFileNames.push_back(argv[++i]);
+			cout<<"Accepting input file name: "<<m_InputFileNames.back()<<endl;
+    } else if (Option == "-o") {
+      m_OutputFileName = argv[++i];
+			cout<<"Accepting output file name: "<<m_OutputFileName<<endl;
+    } else {
+			cout<<"Error: Unknown option \""<<Option<<"\"!"<<endl;
+			cout<<Usage.str()<<endl;
+			return false;
+		}
+  }
+
+  if (m_OutputFileName == "") {
+	  cout<<"Error: Empty output file name!"<<endl;
+    return false;
+  }
+  if (m_InputFileNames.size() == 0) {
+	  cout<<"Error: You need to give some input file names!"<<endl;
+    return false;
+  }
+  for (unsigned int f = 0; f < m_InputFileNames.size(); ++f) {
+    if (MFile::Exists(m_InputFileNames[f]) == false) {
+	    cout<<"Error: File does not exist: "<<m_InputFileNames[f]<<endl;
+      return false;
+    }
+  }
+
+  return true;
+}
+
+
+/******************************************************************************
+ * Do whatever analysis is necessary
+ */
+bool ConcatDat::Analyze()
+{
+  NSatellite Sat;
+  
+  NModuleEventSaver Saver(Sat);
+  Saver.SetFileName(m_OutputFileName);
+  if (Saver.Initialize() == false) return false;
+
+  NTime TimeOffset(0);
+  unsigned int IDOffset = 0;
+  for (unsigned int f = 0; f < m_InputFileNames.size(); ++f) {
+    NModuleEventLoader Loader(Sat);
+    Loader.SetFileName(m_InputFileNames[f]);
+    if (Loader.Initialize() == false) return false;
+    
+    NEvent Event;
+    NTime LatestTime(0);
+    unsigned int LatestID = 0;
+    while (Loader.AnalyzeEvent(Event) == true) {
+      if (Event.IsEmpty() == true) break; 
+      
+      LatestTime = Event.GetTime();
+      LatestID = Event.GetID();
+      Event.SetTime(Event.GetTime() + TimeOffset);
+      Event.SetID(Event.GetID() + IDOffset);
+      Saver.AnalyzeEvent(Event);
+    }
+    TimeOffset += LatestTime;
+    IDOffset += LatestID; 
+    Loader.Finalize();
+  }
+  Saver.Finalize();
+
+  return true;
+}
+
+
+/******************************************************************************/
+
+ConcatDat* g_Prg = 0;
+
+/******************************************************************************/
+
+
+/******************************************************************************
+ * Called when an interrupt signal is flagged
+ * All catched signals lead to a well defined exit of the program
+ */
+void CatchSignal(int a)
+{
+  cout<<"Catched signal Ctrl-C (ID="<<a<<"):"<<endl;
+  
+  if (g_Prg != 0) {
+    g_Prg->Interrupt();
+  }
+}
+
+
+/******************************************************************************
+ * Main program
+ */
+int main(int argc, char** argv)
+{
+  //void (*handler)(int);
+	//handler = CatchSignal;
+  //(void) signal(SIGINT, CatchSignal);
+
+
+  TApplication ConcatDatApp("ConcatDatApp", 0, 0);
+
+  g_Prg = new ConcatDat();
+
+  if (g_Prg->ParseCommandLine(argc, argv) == false) {
+    cerr<<"Error during parsing of command line!"<<endl;
+    return -1;
+  } 
+  if (g_Prg->Analyze() == false) {
+    cerr<<"Error during analysis!"<<endl;
+    return -2;
+  } 
+
+  ConcatDatApp.Run();
+
+  cout<<"Program exited normally!"<<endl;
+
+  return 0;
+}
+
+/*
+ * Cosima: the end...
+ ******************************************************************************/
