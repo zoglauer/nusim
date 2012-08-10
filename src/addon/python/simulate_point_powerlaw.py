1e7b46098e7e58115a2fed5ca3144f0bab96a0c8
Andrew Ptak <andrew.f.ptak@nasa.gov>
2012-08-10 20:37:14 +0000

    Now is much smarter
    git-svn-id: https://elves.srl.caltech.edu/svn/nusim/trunk@603 f61b1093-7add-4ab6-ac1b-eb05caf01ebb

--- src/addon/python/simulate_point_powerlaw.py
+++ src/addon/python/simulate_point_powerlaw.py
@@ -4,12 +4,22 @@ import os, sys
 
 argv = sys.argv
 argc = len(argv)
-
+simulate_bgd = True
+flux = 0.
+flux_Elow = 10.
+flux_Ehi = 30.
+gamma = 1.8
 if argc < 3:
-    print "Usage: %s outconfigfile exptime (flux)" % argv[0]
+    #print "Usage: %s outconfigfile exptime (flux)" % argv[0]
+    print "Usage: %s outconfigfile exptime (flux gamma flux_Elow flux_Ehi)" % argv[0]
     print "Generates configuration to a point source based on default config in "
     print "  $NUSIM/resource/configurations/AstrophysicsMode.cfg"
-    print "Resets exposure time and flux (in photons/s/cm^2, if given) to inputted values"
+    print "Sets exposure time to inputted value"
+    print "If given sets flux (input in ergs/s/cm^2) and photon index,"
+    print "flux in flux_Elow to flux_Ehi range (def. = %.1f - %.1f keV)" % (flux_Elow, flux_Ehi)
+    print "Default photon index = %f" % gamma
+    #print "simulate_bgd = 1 (yes, default) or 0 (no)"
+    
     sys.exit()
 
 if not os.environ.has_key("NUSIM"):
@@ -27,6 +37,28 @@ cfg = nusim_cfg.nusim_cfg(incfgfile)
 outfile = argv[1]
 exptime = argv[2] # leave as a string since we are setting xml fields
 
+str = " not"
+if simulate_bgd:
+    str = ""
+print "Background will%s be simulated" % str
+
+flux = 0.
+if len(argv) > 3:
+    flux = float(argv[3])
+
+if len(argv) > 4:
+    gamma = float(argv[4])
+if len(argv) > 5:
+    flux_Elow = float(argv[5])
+if len(argv) > 6:
+    flux_Ehi = float(argv[6])
+
+# Get power-law norm
+norm = nusim_cfg.pl_norm(flux_Elow, flux_Ehi, gamma, flux)
+photflux = nusim_cfg.pl_photflux(3., 82., gamma, norm)
+print "Power-law norm to give F(%.1f-%.1f) = %g: %g" % (flux_Elow, flux_Ehi, flux, norm)
+print "3-82 keV photon flux = %g" % photflux
+
 # Make sure there is only one pointing here
 if len(cfg.pointings_list) != 1:
     print "Expected only one pointing, got %d" % len(cfg.pointings_list)
@@ -36,17 +68,28 @@ if len(cfg.pointings_list) != 1:
 cfg.pointings_list[0].find("Time").text = exptime
 cfg.find("ObservationTime").text = exptime
 
+srcname = "pointsrc_flux%g" % flux
+# Update source
+cfg.src_list[0].find("Name").text = srcname
+cfg.src_list[0].find("EnergyParam1").text = "%g" % 3.
+cfg.src_list[0].find("EnergyParam2").text = "%g" % 82.
+cfg.src_list[0].find("EnergyParam3").text = "%g" % gamma
+cfg.src_list[0].find("Flux").text = "%g" % (photflux/100.)
+
+cfg.find("TargetName").text = srcname
+rootname = srcname
+#if not simulate_bgd:
+#    srcname += "_nobgd"
+cfg.find("BaseFileName").text = srcname
+cfg.find("SimulateOccultations").text = "false"
+
 # Actually save events list
 cfg.find("ModuleOptions/XmlTagEventSelector/SaveEventsAsFits").text = "true"
-
-# "bug" fix
-cfg.find("ModuleOptions/XmlTagModuleOrbitEngineTLE/TLEFileName").text = \
-     nusim_root + "resource/data/Launch.tle"
 
 cfg.write(outfile)
 
 # Actually run nusim
-com = nusim_root + "bin/nusim -c " + outfile + " -a"
+com = nusim_root + "/bin/nusim -c " + outfile + " -a"
 import popen2
 proc = popen2.Popen4(com)
 inf = proc.fromchild
